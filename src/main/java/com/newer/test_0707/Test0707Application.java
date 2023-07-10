0018eecc9b5359620928b3fdbdb7152564fa1ca9
liuqiaodan <2107721396@qq.com>
2023-07-10 18:37:55 +0800

    Initial commit

---
+++ src/main/java/com/newer/test_0707/Test0707Application.java
@@ -0,0 +1,13 @@
+package com.newer.test_0707;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0707Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test0707Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/newer/test_0707/Test0707ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newer.test_0707;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0707ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
